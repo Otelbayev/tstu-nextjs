0bdb9e15b0ba45a40b0b1d30a725f31a5449ff92
jasurbek <jasurdev1604@gmail.com>
2025-10-02 01:58:36 +0500

    hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,16 +6,27 @@ const Hero = () => {
     <div className="relative w-full" style={{ height: "calc(100dvh - 56px)" }}>
       <Image
         src="/icons/hero/1.png"
-        alt="Hero background"
+        alt="Hero background desktop"
         fill
         priority
         quality={80}
-        className="object-cover"
+        className="object-cover hidden md:block"
       />
+
+      <Image
+        src="/icons/hero/2.png"
+        alt="Hero background mobile"
+        fill
+        priority
+        quality={100}
+        className="object-cover mt-15 z-5 md:hidden"
+      />
+
+      <div className="absolute inset-0 md:hidden bg-gradient-to-b from-[#0061A5] to-[#004B7D]"></div>
 
       <div className="relative z-10 flex items-center justify-center h-full text-white">
         <h1
-          className="text-6xl md:text-7xl xl:text-8xl text-center py-4"
+          className="text-6xl md:text-7xl xl:text-8xl text-center px-4"
           style={{ fontFamily: "var(--font-canela), serif" }}
         >
           Transport Universiteti
