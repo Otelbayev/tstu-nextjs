d96212617aee6d4b5cc37bac73c8e689b1a2761a
jasurbek <jasurdev1604@gmail.com>
2025-10-02 02:17:19 +0500

    hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,36 +3,33 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="relative w-full" style={{ height: "calc(100dvh - 56px)" }}>
+    <section
+      className="relative w-full"
+      style={{ height: "calc(100dvh - 56px)" }}
+    >
       <Image
         src="/icons/hero/1.png"
         alt="Hero background desktop"
         fill
         priority
-        quality={80}
-        className="object-cover hidden md:block"
+        quality={75}
+        sizes="100vw"
+        className="object-cover"
       />
 
-      <Image
-        src="/icons/hero/2.png"
-        alt="Hero background mobile"
-        fill
-        priority
-        quality={100}
-        className="object-cover mt-15 z-5 md:hidden"
-      />
-
-      <div className="absolute inset-0 md:hidden bg-gradient-to-b from-[#0061A5] to-[#004B7D]"></div>
+      <div className="absolute inset-0 md:hidden bg-gradient-to-b from-[#0061A5] to-[#004B7D] opacity-50"></div>
 
       <div className="relative z-10 flex items-center justify-center h-full text-white">
         <h1
-          className="text-6xl md:text-7xl xl:text-8xl text-center px-4"
-          style={{ fontFamily: "var(--font-canela), serif" }}
+          className="text-5xl md:text-7xl xl:text-8xl text-center px-4"
+          style={{
+            fontFamily: "var(--font-canela), serif",
+          }}
         >
           Transport Universiteti
         </h1>
       </div>
-    </div>
+    </section>
   );
 };
 
